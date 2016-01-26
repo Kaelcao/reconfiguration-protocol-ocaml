2dc9e42e85136d70cba463d09fd55bd4404d05c6
Cao Anh Quan <aquancva@gmail.com>
2016-01-26 14:54:47 +0700

    done propagation rules

--- reconfiguration.ml
+++ reconfiguration.ml
@@ -7,13 +7,20 @@ type wire = {src:string;dst:string;wire_type:string}
 	all component in stopped state will be stored in stopped_components array
 	all component in failed state will be stored in started_components array
 *)
-let started_components = ref ["a"];;
-let failed_components = ref ["b"];;
-let stopped_components = ref ["c"];;
-let wires = ref [{src="a";dst="b";wire_type="c"}];;
 
 let mandatory_state = "mandatory";;
 let optional_state = "optional";;
+
+let started_components = ref ["d";"c";"c1";"c2"];;
+let failed_components = ref [];;
+let stopped_components = ref [];;
+
+let w1 =  {src="d";dst="c";wire_type=mandatory_state};;
+let w2 = {src="c1";dst="c";wire_type=optional_state};;
+let w3 = {src="c";dst="c1";wire_type=mandatory_state};;
+
+let wires = ref [w1;w2;w3];;
+
 
 (* 
 ---------helper functions--------- ssss
@@ -71,14 +78,6 @@ let show_wire = print_wire_list !wires;;
 *)
 let construct component = (stopped_components := (!stopped_components@[component]));;
 
-
-
-(*
-	Destruct a component
-	Remove component from the stopped_components list
-*)
-let destruct component = (stopped_components := (remove component !stopped_components));;
-
 (*
 	Start a component
 	Remove component from the stopped state
@@ -97,6 +96,13 @@ let start component =
 let stop component = 
 	(stopped_components := (!stopped_components@[component]));
 	(started_components := (remove component !started_components));;
+
+(*
+	Destruct a component
+	Remove component from the stopped_components list
+*)
+let destruct component = stop component;(stopped_components := (remove component !stopped_components));;
+
 (*
 	Create a wire between 2 component 
 	Create wire with its state
@@ -110,6 +116,45 @@ let wire com_src com_dst wire_state = (wires := (!wires@[{src=com_src;dst=com_ds
 *)
 let rec unwire wire = (wires := (remove_wire wire !wires));;
 
-
 (* --------- End Reconfiguration Operations --------- *)
+
+
+
+(* ---------- Begin Propagation rule helper functions ----------*)
+
+let mandatory wire = (wire.wire_type = mandatory_state);;
+
+(* ---------- End Propagation rule helper functions ----------*)
+
+
+
+(* --------- Begin Propagation Rules --------- *)
+
+let rec stopped c = 
+	let rec stopped_rec c w =
+		match w with
+		|[]->()
+		|w::t -> if (w.dst=c) then
+				if (mandatory w) then stopped w.src
+				else unwire w;
+				stopped_rec c t
+	in stopped_rec c !wires;
+	stop c;;
+
+let unwired w = 
+	if (mandatory w) then 
+	stopped(w.src);
+	unwire w;;
+
+let destructed c =
+	let rec destructed_rec c w =
+		match w with
+		|[] -> ()
+		| w::t -> if (w.src=c || w.dst=c) then unwired w; destructed_rec c t
+	in destructed_rec c !wires;
+	destruct c;;
+
+
+(* --------- End Propagation Rules --------- *)
+
 
